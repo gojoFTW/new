5903c47c593b8674abcf405fdbaea85dd177f0b6
gojoFTW <104652822+gojoFTW@users.noreply.github.com>
2024-09-07 23:16:48 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7230502558:AAGOKrjx-W7JpLewWfgYko2bRjLk-LhYD9I")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("username")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6444435371")
+
+
+
+
+
+
+
+  
